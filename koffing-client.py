63bd64533609ef097ad194bc93432eb31b718ce2
Zach Greenhalge <Zachary.Greenhalge@gmail.com>
2016-12-07 16:42:53 -0500

    Updates
    added timestamps and other niceties (like commands that actually work)

--- koffing-client.py
+++ koffing-client.py
@@ -1,23 +1,27 @@
 import discord
 import asyncio
+import sys
+import datetime
+import time
 from random import randint
 
 dan_id = '245295076865998869'
-start_messages = ["Koffing-bot, go!", "Get 'em Koffing-bot!"]
+start_messages = ["Koffing-bot, go~!", "Get 'em Koffing-bot~!"]
+dev = True
 
 client = discord.Client()
 
 @client.event
 @asyncio.coroutine
 def on_ready():
-    print('Logged in as')
-    print('{} ({}) - {}'.format(client.user.name, client.user.display_name, client.user.id))
-    print('------')
+    print('{} Logged in as {} ({}) - {}'.format(get_time_str(), client.user.name, client.user.display_name, client.user.id))
+    if dev==True:
+        return
     for server in client.servers:
         if server.id==dan_id:
             for channel in server.channels:
                 if channel.type==discord.ChannelType.text:
-                    print('  Alerting {} to bot presence'.format(channel.name))
+                    print('{}  Alerting {} to bot presence'.format(get_time_str(), channel.name))
                     yield from client.send_message(channel, start_messages[randint(0, len(start_messages)-1)])
                 
 
@@ -26,30 +30,33 @@ def on_ready():
 def on_message(message):
     if message.server.id!=dan_id:
         return
-    print('Recieved message from {} ({}) in {}::{}'.format(message.author.display_name, message.author.name, message.server.name, message.channel.name))
-    if message.author.id==client.user.id:
+    print('{} Recieved message from "{}" ({}) in {}::{}'.format(get_time_str(), message.author.display_name, message.author.name, message.server.name, message.channel.name))
+
+    if message.author.id==client.user.id and dev==False:
         return
     if message.content.startswith('!test'):
-        yield from client.send_message(message.channel, 'Okay @{}, relax...'.format(message.author.name))
+        yield from client.send_message(message.channel, 'Okay {}, relax...'.format(message.author.mention))
 
     elif message.content.startswith('!koffing'):
         yield from asyncio.sleep(randint(0,5))
-        print("  Responding to {} ({}) in {}".format(message.author.display_name, message.author.name, message.channel.name))
+        print('{}   Responding to "{}" ({}) in {}'.format(get_time_str(), message.author.display_name, message.author.name, message.channel.name))
         yield from client.send_message(message.channel, 'Koffing!!')
 
-    elif message.content.startswith('!return'):
-        shutdown_message()
+    elif message.content.startswith('!kill'):
+        yield from shutdown_message()
         yield from client.logout()
-        sys.exit()
 
+@asyncio.coroutine
 def shutdown_message():
     for server in client.servers:
         if server.id==dan_id:
             for channel in server.channels:
                 if channel.type==discord.ChannelType.text:
-                    print('  Alerting {} to bot shutdown'.format(channel.name))
-                    yield from client.send_message(channel.id, 'Koffing-bot is going back to its pokeball!')
+                    print('{}  Alerting {} to bot shutdown'.format(get_time_str(), channel.name))
+                    yield from client.send_message(channel, 'Koffing-bot is going back to its pokeball~!')
 
+def get_time_str():
+    return '[{}]'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d-%Y %H:%M:%S'))
 
 # client.run('token')
 client.run('zgreenhalge@gmail.com', 'seraphim')
