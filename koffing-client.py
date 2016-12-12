8ac5a60caf4a17f1a484764b53feb7d448ba5e3e
Zach Greenhalge <Zachary.Greenhalge@gmail.com>
2016-12-12 13:44:41 -0500

    Settings now persist
    Write authorized servers & channels, muted channels, and admin users to
    disk on (clean) shutdown

--- koffing-client.py
+++ koffing-client.py
@@ -4,25 +4,20 @@ import sys
 import datetime
 import time
 import logging
+import json
 from random import randint
 
-# OOOUGH (219600948111212544)
-# Watchpoint: Dorado (205773751206281216)
-# Discord API (81384788765712384)
-# Mapleworld Here we Come (215242452284604417)
-# Discord is cool kids (182282826010460160)
 # Bot Testing (256173272637374464)
 # Dan (245295076865998869)
 
 LOG_FORMAT = '[%(asctime)-15s] [%(levelname)s] - %(message)s'
-token = 'MjU2MjIyMjU4NjQ3OTI0NzM3.CypBIw.c3RDGrECBWYVwV77aN_2o0j8BkU'
-FEATURE_LIST = '```Current feature list (*=requires privilege):\n -responds in text channels!\n -responds in voice channels (NOT IMPLEMENTED)\n -roll call (NOT IMPLEMENTED)\n -song of the day (NOT IMPLEMENTED)\n -elo lookup [Overwatch] (NOT IMPLEMENTED) \n -elo lookup [LoL] (NOT IMPLEMENTED) \n -mute```'
-HELP = 'Koffing~~ I will respond any time my name is called!```\nCommands (*=requires privilege):\n !koffing help\n !koffing features\n*!koffing mute\n*!koffing unmute```'
+TOKEN = 'MjU2MjIyMjU4NjQ3OTI0NzM3.CypBIw.c3RDGrECBWYVwV77aN_2o0j8BkU'
+FEATURE_LIST = '```Current feature list (*=requires privilege):\n -responds in text channels!\n -responds in voice channels (PLANNED)\n -roll call (PLANNED)\n -song of the day (PLANNED)\n -elo lookup [Overwatch] (PLANNED) \n -elo lookup [LoL] (PLANNED) \n -mute\n -vote (PLANNED)```'
+HELP = 'Koffing~~ I will respond any time my name is called!```\nCommands (*=requires privilege):\n /koffing help\n /koffing features\n*/koffing mute\n*/koffing unmute```'
+CONFIG = 'koffing.cfg'
 start_messages = ["Koffing-bot, go~!", "Get 'em Koffing-bot~!"]
 dev = True
 #--------------------------------------------------------------------
-client = discord.Client()
-
 #Logging set up
 date = datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d-%Y')
 logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
@@ -33,16 +28,13 @@ fh.setFormatter(logging.Formatter(LOG_FORMAT))
 logger.addHandler(fh)
 
 #Control lists
+settings = json.load(open(CONFIG))
+authorized_servers = settings['authorized_servers']
+authorized_channels = settings['authorized_channels']
+muted_channels = settings['muted_channels']
+admin_users = settings['admin_users']
 
-authorized_servers = ['256173272637374464'] #Bot Testing only
-authorized_channels_testing = ['test']
-authorized_channels_dan = ['dan_lounge']
-
-authorized_channels = {'256173272637374464': authorized_channels_testing, '245295076865998869': authorized_channels_dan}
-
-muted_channels = {}
-
-admin_users = ['Grachary']
+client = discord.Client()
 #--------------------------------------------------------------------
 
 @client.event
@@ -58,16 +50,16 @@ def on_ready():
     for server in client.servers:
         if server.id in authorized_servers:
             for channel in server.channels:
-                if channel.type==discord.ChannelType.text:
-                    if authorized(server, channel):
-                        logger.info('Alerting %s::%s to bot presence', server.name, channel.name)
-                        yield from client.send_message(channel, start_messages[randint(0, len(start_messages)-1)])
+                if channel.type==discord.ChannelType.text and can_message(server, channel):
+                    logger.info('Alerting %s::%s to bot presence', server.name, channel.name)
+                    yield from client.send_message(channel, start_messages[randint(0, len(start_messages)-1)])
                 
 
 @client.event
 @asyncio.coroutine
 def on_message(message):
     logger.debug('Recieved message from "%s" (%s) in %s::%s', message.author.display_name, message.author.name, message.server.name, message.channel.name)
+    
     if not authorized(message.server, message.channel):
         return
     if message.author.id==client.user.id:
@@ -77,41 +69,73 @@ def on_message(message):
     channel = message.channel
     content = message.content
     author = message.author
-    if(content.startswith('!koffing ')):
-        content = content.replace('!koffing ', '', 1)
-        if content.startswith('test') and not muted(server, channel):
-            yield from client.send_message(channel, 'Okay {}, relax...'.format(author.mention))
 
-        if content.startswith('help') and not muted(server, message):
+    if(content.startswith('/koffing ')):
+        content = content.replace('/koffing ', '', 1) #remove the first '/koffing' from the string for easier parsing of command
+
+        elif content.startswith('help') and not muted(server, message):
             yield from client.send_message(channel, HELP)        
 
-        if content.startswith('feature') and not muted(server, channel):
+        elif content.startswith('feature') and not muted(server, channel):
             yield from client.send_message(channel, FEATURE_LIST)
 
-        if content.startswith('mute') and privileged(author.name):
-            if not muted(server, channel):
-                yield from client.send_message(channel, "Koffing...")
-                mute(server, channel)
+        elif content.startswith('admin'):
+            content = content.replace('admin ', '', 1)
+            if not privileged(author) and not muted(server, channel):
+                yield from client.send_message(channel, "I'm afraid you can't do that {}".format(author.mention))
+            elif content.startswith('list') and not muted(server, channel):
+                yield from client.send_message(channel, get_admin_list())
+            elif content != '' and content not in admin_users:
+                admin_users.append(content)
 
-        if content.startswith('unmute') and privileged(author.name):
-            if muted(server, channel):
-                response, emoji = generate_koffing(server)
-                yield from client.send_message(channel, response)
+        elif content.startswith('mute'):
+            content = content.replace('mute ', '', 1)
+            if not privileged(author) and not muted(server, channel):
+                yield from client.send_message(channel, "I'm afraid you can't do that {}".format(author.mention))
+            else:
+                mute(server, channel)
+                if not muted(server, channel):
+                    yield from client.send_message(channel, "Koffing...")
+
+        elif content.startswith('unmute'):
+            if not privileged(author) and not muted(server, channel):
+                yield from client.send_message(channel, "I'm afraid you can't do that {}".format(author.mention))
+            else:
+                if muted(server, channel):
+                    response, emoji = generate_koffing(server)
+                    yield from client.send_message(channel, response)
                 unmute(server, channel)
 
-        elif content.startswith('return') and privileged(author.name):
+        elif content.startswith('return') and privileged(author):
             yield from shutdown_message()
             yield from client.logout()
+
+        elif not muted(server, channel):
+            yield from client.send_message(channel, "Koff koff~ \nInvalid command, please use /koffing help for usage")
     else:
         yield from check_for_koffing(message)
 
 @asyncio.coroutine
 def shutdown_message():
+    save_config()
     for server in client.servers:
         for channel in server.channels:
             if channel.type==discord.ChannelType.text and can_message(server, channel):
                 logger.info('Alerting %s::%s to bot shutdown', server.name, channel.name)
                 yield from client.send_message(channel, 'Koffing-bot is going back to its pokeball~!')              
+
+def save_config():
+    logger.info('Writing settings to file...')
+    file = open(CONFIG, 'w')
+    json_str = json.dumps({'authorized_channels': authorized_channels, 'authorized_servers': authorized_servers, 'muted_channels': muted_channels, 'admin_users': admin_users})
+    file.write(json_str)
+    file.close()
+
+def get_admin_list():
+    admin_str = 'Trainers that I listen to:\n'
+    for user in admin_users:
+        admin_str += ' -' + user + '\n'
+    return admin_str
 
 @asyncio.coroutine
 def check_for_koffing(message):
@@ -133,8 +157,8 @@ def check_for_koffing(message):
 def can_message(server, channel):
     return authorized(server, channel) and not muted(server, channel)
 
-def privileged(name):
-    return name in admin_users
+def privileged(user):
+    return user.mention in admin_users
 
 def authorized(server, channel):
     if server.id in authorized_servers:
@@ -143,20 +167,20 @@ def authorized(server, channel):
 
 def muted(server, channel):
     if server.id in muted_channels:
-        return channel.id in muted_channels[server.id]
+        return channel.name in muted_channels[server.id]
     return False
 
 def mute(server, channel):
     if server.id in muted_channels:
-        if channel.id not in muted_channels[server.id]:
-            muted_channels[server.id].append(channel.id)
+        if channel.name not in muted_channels[server.id]:
+            muted_channels[server.id].append(channel.name)
     else:
-        muted_channels[server.id] = [channel.id]
+        muted_channels[server.id] = [channel.name]
 
 def unmute(server, channel):
     if server.id in muted_channels:
-        if channel.id in muted_channels[server.id]:
-            muted_channels[server.id].remove(channel.id)
+        if channel.name in muted_channels[server.id]:
+            muted_channels[server.id].remove(channel.name)
 
 def generate_koffing(server):
     koffing_emoji = None
@@ -173,4 +197,4 @@ def generate_koffing(server):
         reponse = 'Koff' + randint(1,5)*'i' + 'ng' + randint(1,5)*'!'
     return response, koffing_emoji
 
-client.run(token)
+client.run(TOKEN)
