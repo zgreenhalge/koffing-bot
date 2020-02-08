7bafc6e23ba004db382e92815300f1e3063c7818
Zach Greenhalge <zgreenhalge@gmail.com>
2020-02-07 20:18:19 -0500

    Update koffing-ball.py
    Pull latest code before restarting

--- koffing-ball.py
+++ koffing-ball.py
@@ -6,6 +6,10 @@ def start_koffing():
 
 ret_code = start_koffing()
 while ret_code == 0:
+  ret_code = os.system("git pull")
+  if ret_code > 0:
+    print("Update failed. Git pull return code %d" % ret_code)
   ret_code = start_koffing()
 
 print("Koffing exited with code %d" % ret_code)
+
