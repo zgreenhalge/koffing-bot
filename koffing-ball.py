abcc5a6f166a276ea750c44026b34c83e6b869fc
greez <Zachary.Greenhalge@pega.com>
2020-02-10 12:52:20 -0500

    Update koffing-ball.py
    Try to make koffing-ball smarter about OS & return value differences

--- koffing-ball.py
+++ koffing-ball.py
@@ -1,12 +1,18 @@
 import sys
 import os
 
+def run_and_ret_exit_code(command):
+	ret_value = os.system(command)
+	if not sys.platform.startswith('win'):
+		ret_value = ret_value >> 8
+	return ret_value
+
 def start_koffing():
-  return os.system("./launchKoffing.sh")
+  return run_and_ret_exit_code("./launchKoffing.sh")
 
 ret_code = start_koffing()
 while ret_code == 0:
-  ret_code = os.system("git pull")
+  ret_code = run_and_ret_exit_code("git pull")
   if ret_code > 0:
     print("Update failed. Git pull return code %d" % ret_code)
   ret_code = start_koffing()
